e0756d03ee352ae25efcb34be6f0963bdf2f4b29
Hamza Idrees <hamzaidrees083@gmail.com>
2023-11-12 13:04:57 +0500

    FYP-10 Migration driver_details

---
+++ migrations/20231111225522-create_driver_details_table.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('driverDetails', {
+      driver_id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        allowNUll: false,
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      cnic: {
+        type: Sequelize.STRING
+      },
+      liscence: {
+        type: Sequelize.BLOB
+      },
+      inspection: {
+        type: Sequelize.BLOB
+      }
+    });
+
+    await queryInterface.addConstraint('driverDetails', {
+      fields: ['driver_id'],
+      type: 'foreign key',
+      name: 'fk_driver_id',
+      references: {
+        table: 'drivers',
+        field: id,
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('driverDetails');
+  }
+};
