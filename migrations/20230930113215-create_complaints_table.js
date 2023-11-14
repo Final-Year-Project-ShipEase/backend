691f5a93533e2842b7fe3a01d75115200a5f8627
Hamza Idrees <hamzaidrees083@gmail.com>
2023-11-15 01:23:47 +0500

    FYP-10 Update Migration complaints_table

--- migrations/20230930113215-create_complaints_table.js
+++ migrations/20230930113215-create_complaints_table.js
@@ -7,6 +7,10 @@ module.exports = {
         autoIncrement: true,
         allowNull: false,
       },
+      admin_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
       status: {
         type: Sequelize.ENUM('pending', 'resolved'),
       },
@@ -21,9 +25,22 @@ module.exports = {
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
       },
     });
+
+    await queryInterface.addConstraint('complaints', {
+      fields: ['admin_id'],
+      type: 'foreign key',
+      name: 'fk_admin_id',
+      references: {
+        table: 'admins',
+        field: 'id',
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    });
   },
 
   down: async (queryInterface) => {
+    await queryInterface.removeConstraint('complaints', 'fk_admin_id')
     // Drop the complaints table
     await queryInterface.dropTable('complaints');
   },
