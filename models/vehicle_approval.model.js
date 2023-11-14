25aabd208825066585a4bddc759de608a063bc79
Hamza Idrees <hamzaidrees083@gmail.com>
2023-11-15 01:43:33 +0500

    FYP-10 Model vehicle_approval_table

---
+++ models/vehicle_approval.model.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require('sequelize');
+module.exports = (sequelize) => {
+  const VehicleApproval = sequelize.define('vehicleApproval', {
+    vehicle_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+    },
+    tenant_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM('approved', 'rejected'),
+    },
+    status: {
+        type: DataTypes.ENUM('active', 'closed'),
+      },
+      admin_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+  });
+
+  VehicleApproval.associate = (models) => {
+    VehicleApproval.belongsTo(models.Vehicle, { foreignKey: 'driver_id' });
+    VehicleApproval.belongsTo(models.Admin, { foreignKey: 'admin_id' });
+    VehicleApproval.belongsTo(models.Tenant, { foreignKey: 'tenant_id' });
+  };
+  return VehicleApproval;
+};
