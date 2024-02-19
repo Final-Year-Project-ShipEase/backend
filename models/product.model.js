9b1a897e9539cd0c51ed2259f62eeb899b51cfd6
M-Muntazer-Mehdi <muntazer.mehdi.rizvi@gmail.com>
2024-02-19 17:49:18 +0500

    product model file

---
+++ models/product.model.js
@@ -0,0 +1,28 @@
+// models/product.js 
+const { DataTypes } = require('sequelize');
+module.exports = (sequelize) => { 
+    const Product = sequelize.define('product', { 
+        id: { 
+            type: DataTypes.INTEGER, 
+            primaryKey: true, 
+            autoIncrement: true, 
+            allowNull: false, 
+        }, 
+        name: { 
+            type: DataTypes.STRING, 
+            allowNull: false, 
+        }, 
+        image: { 
+            type: DataTypes.STRING, 
+            allowNull: false, 
+        }, 
+        price: { 
+            type: DataTypes.FLOAT, 
+            allowNull: false, 
+        }, 
+        description: { 
+            type: DataTypes.TEXT, 
+        },
+     });
+
+return Product; };
