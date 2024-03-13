ac43ebd1d1d13b4cd15887ce08c542cbd54a0d27
Abdullah Minhas <aminhas309@hotmail.com>
2024-03-13 23:55:01 +0500

    FYP-39 heruko deployment changes

--- config/db.js
+++ config/db.js
@@ -4,6 +4,16 @@ const { env } = require('../utils/constants');
 const config = require('./config');
 const { NODE_ENV } = env;
 
-const DBConnection = new Sequelize(config[NODE_ENV]);
+let sequelize;
+if (config[NODE_ENV].use_env_variable) {
+  // For production environment, use the DATABASE_URL directly
+  sequelize = new Sequelize(process.env[config[NODE_ENV].use_env_variable], config[NODE_ENV]);
+} else {
+  // For other environments, use the existing configuration
+  sequelize = new Sequelize(config[NODE_ENV]);
+}
+
+const DBConnection = sequelize;
+
 
 module.exports = DBConnection;
