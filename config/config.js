77bc2cd8b0ac9b711f0efd0b88eb8adf5e82eef9
Abdullah Minhas <aminhas309@hotmail.com>
2024-03-13 23:53:03 +0500

    heruko deployment changes

--- config/config.js
+++ config/config.js
@@ -16,10 +16,14 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: process.env.DB_DIALECT,
+    protocol: process.env.DB_DIALECT,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
