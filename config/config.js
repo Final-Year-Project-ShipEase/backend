93f352b0ec9d3a3bd1b58edabb8e3ec1183484be
minhas309 <aminhas309@hotmail.com>
2024-03-25 23:24:53 +0500

    changes

--- config/config.js
+++ config/config.js
@@ -17,12 +17,12 @@ module.exports = {
   },
   production: {
     // heroku config vars for production
-    use_env_variable: process.env.DATABASE_URL,
+    use_env_variable:
+      'postgres://aytubncgvakpug:a2ad2ad7cc2c0987fd91ea6425df212f5517302d2cac229411897c02bcf5efc0@ec2-107-21-67-46.compute-1.amazonaws.com:5432/df6mttapos0aml',
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
       ssl: {
-        require: true,
         rejectUnauthorized: false,
       },
     },
