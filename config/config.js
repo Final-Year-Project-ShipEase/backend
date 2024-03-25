79f467bb5c93df4a2e0261d296b678f7209ff0e5
minhas309 <aminhas309@hotmail.com>
2024-03-25 23:05:05 +0500

    changes

--- config/config.js
+++ config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
