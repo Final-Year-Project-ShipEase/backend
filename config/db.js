43c74c782756238a5f710a2a6267f39ed257c126
minhas309 <aminhas309@hotmail.com>
2024-03-25 23:49:22 +0500

    changes

--- config/db.js
+++ config/db.js
@@ -1,19 +1,32 @@
 const { Sequelize } = require('sequelize');
-
-const { env } = require('../utils/constants');
 const config = require('./config');
-const { NODE_ENV } = env;
+require('dotenv').config();
+
+// Assuming NODE_ENV will be either 'development', 'test', or 'production'
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 let sequelize;
-if (config[NODE_ENV].use_env_variable) {
-  // For production environment, use the DATABASE_URL directly
-  sequelize = new Sequelize(process.env[config[NODE_ENV].use_env_variable], config[NODE_ENV]);
+
+// Check if the environment is 'production' and if 'use_env_variable' is specified in the config
+if (NODE_ENV === 'production' && config[NODE_ENV].use_env_variable) {
+  // For Heroku or other production environments where DATABASE_URL is provided
+  sequelize = new Sequelize(process.env[config[NODE_ENV].use_env_variable], {
+    ...config[NODE_ENV],
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
 } else {
-  // For other environments, use the existing configuration
-  sequelize = new Sequelize(config[NODE_ENV]);
+  // For non-production environments or when DATABASE_URL is not used
+  sequelize = new Sequelize({
+    ...config[NODE_ENV],
+    logging: false, // Optionally turn off Sequelize logging in development
+  });
 }
 
 const DBConnection = sequelize;
-
 
 module.exports = DBConnection;
