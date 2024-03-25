76672927f50b4fc1413eb7a784f3ee12994d19d5
minhas309 <aminhas309@hotmail.com>
2024-03-25 23:16:08 +0500

    hello

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
   },
   production: {
     // heroku config vars for production
-    use_env_variable: DATABASE_URL,
+    use_env_variable: process.env.DATABASE_URL,
     dialect: 'postgres',
     protocol: 'postgres',
     dialectOptions: {
