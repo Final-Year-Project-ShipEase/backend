d35856c7744973dc712d4aff0128ccdde571dd09
minhas309 <aminhas309@hotmail.com>
2024-03-26 00:03:07 +0500

    port changes

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ validateRequiredEnvVars(process.env);
   }
 })();
 
-const port = process.env.BACKEND_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => print(`Server is Running at Port: ${port}`));
