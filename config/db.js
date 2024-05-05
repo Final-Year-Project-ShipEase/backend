099504b32ebab7e6557478640b32451babdebb7d
Debian <bitnami@ip-172-26-13-110.ap-south-1.compute.internal>
2024-05-05 19:18:27 +0000

    Deployed server

--- config/db.js
+++ config/db.js
@@ -7,26 +7,11 @@ const NODE_ENV = process.env.NODE_ENV || 'production';
 
 let sequelize;
 
-if (NODE_ENV === 'production') {
-  sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  });
-  console.log('Connected to production database');
-  console.log('NODE_ENV:', process.env.DATABASE_URL);
-} else {
   // For non-production environments or when DATABASE_URL is not used
   sequelize = new Sequelize({
-    ...config[NODE_ENV],
-    logging: false, // Optionally turn off Sequelize logging in development
+    ...config[NODE_ENV]
   });
-}
+
 
 const DBConnection = sequelize;
 
--- migrations/20230930110912-create_vehicles_table.js
+++ migrations/20230930110912-create_vehicles_table.js
@@ -34,7 +34,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       image: {
-        type: DataTypes.BLOB,
+        type: Sequelize.BLOB,
       },
       createdAt: {
         allowNull: false,
