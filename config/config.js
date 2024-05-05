35f7c96a2449c06055a88bedbe9d760ae89f5b63
minhas309 <aminhas309@hotmail.com>
2024-05-05 23:35:38 +0500

    updated config

--- config/config.js
+++ config/config.js
@@ -16,13 +16,10 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL,
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
 };
